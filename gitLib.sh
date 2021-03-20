80996a04f62b3eba69ba6adcdb5b1c143be974e3
jorgealziro <47708374+jorgealziro@users.noreply.github.com>
2021-03-20 11:00:53 -0300

    Update gitLib.sh

--- gitLib.sh
+++ gitLib.sh
@@ -4,7 +4,7 @@
 #REPO=$1
 #BRANCH=$3
 
-#script must be sources so that the function can be called directly
+#script must be sourced so that the function can be called directly
 clonaRepo() {
     git clone --branch $3 https://github.com/$2/$1.git
 }
